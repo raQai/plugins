62df15871a53c4722506ad8149cc7ab6b577e216
Patrick Bogdan <patrick.bgdn@gmail.com>
2015-02-26 16:41:26 +0100

    [TICKETS] fixed a bug that was caused by an empty array of tickets

--- kf-event-manager/classes/MetaBoxBooking.php
+++ kf-event-manager/classes/MetaBoxBooking.php
@@ -17,46 +17,53 @@ class MetaBoxBooking extends MetaBox_Abstract
 		
 		$tickets = $wpdb->get_results( $ticket_query, ARRAY_A );
 
-		$settings = array(
+		if ( empty( $tickets ) ) {
+			$settings = array(
+				'is_active' => 0,
+				'type' => 0, 
+				'string' => ''	
+			);
+		} else {
+			$settings = array(
 				'is_active' => get_post_meta( $post->ID, $this->fields[0], true ),
 				'type' => get_post_meta( $post->ID, $this->fields[1], true ),
 				'string' => $this->kfem_implode( $tickets )
-		);
-		
+			);
+		}	
 		?>
 		<div class="kf-meta-box-checkbox">
-			<input onClick="kfem_checkboxDivDisplay( this.id, 'kf-em-booking' );kfem_createBookingString();" <?php if ( $settings['is_active'] ) echo 'checked '; ?>type="checkbox" id="<?php echo $this->fields[0]?>" name="<?php echo $this->fields[0]?>" value="1" />
+			<input onClick="kfem_checkboxDivDisplay( this.id, 'kf-em-booking' );kfem_createBookingString();" <?php if ( $settings['is_active'] ) echo 'checked '; ?>type="checkbox" id="<?php echo $this->fields[0]?>" name="<?php echo $this->fields[0]?>" value="1" >
 			<label id="kf-em-booking-checkbox-label" for="kfem-booking-checkbox">Voranmeldungen aktivieren</label>
 		</div>
-		<div id="kf-em-booking" <?php if ( !$settings['is_active'] ) echo 'style="display:none" '; ?>>
+		<div id="kf-em-booking" <?php if ( !$settings['is_active'] ) echo 'style="display:none" '; ?> >
 			<div class="kf-em-ticket-type">
 				<p class="kf-em-description">Typ des Turniers:</p>
-				<input onChange="kfem_changeLabelText( 'kf-em-ticket-label-max', 'Max. Teams' );kfem_createBookingString();" required <?php if ( $settings['type'] != 1 ) echo 'checked '; ?>type="radio" name="<?php echo $this->fields[1]; ?>" value="2" />
+				<input onChange="kfem_changeLabelText( 'kf-em-ticket-label-max', 'Max. Teams' );kfem_createBookingString();" required <?php if ( $settings['type'] != 1 ) echo 'checked '; ?>type="radio" name="<?php echo $this->fields[1]; ?>" value="2" >
 				<lable for="kf-em-ticket-type-double">Doppel</lable>
-				<input onChange="kfem_changeLabelText( 'kf-em-ticket-label-max', 'Max. Teilnehmer' );kfem_createBookingString();" required <?php if ( $settings['type'] == 1 ) echo 'checked '; ?>type="radio" name="<?php echo $this->fields[1]; ?>" value="1" />
+				<input onChange="kfem_changeLabelText( 'kf-em-ticket-label-max', 'Max. Teilnehmer' );kfem_createBookingString();" required <?php if ( $settings['type'] == 1 ) echo 'checked '; ?>type="radio" name="<?php echo $this->fields[1]; ?>" value="1" >
 				<lable for="kf-em-ticket-type-single">Einzel</lable>
 				<p class="kf-em-info">Bitte Doppel- und Einzeldisziplinen in getrennten Veranstaltungen erstellen.</p>
 			</div>
 			<div class="kf-em-ticket-header">
 				<label>Name</label>
 				<label>Preis in &euro;</label>
-				<!-- check for kf-em-ticket-type -->
 				<label id="kf-em-ticket-label-max" >Max. <?php echo ( ( $settings['type'] != 1 ) ? 'Teams' : 'Teilnehmer' ); ?></label>
 			</div>
 			<div id="kf-em-tickets">
 				<?php 
-				foreach ( $tickets as $ID => $ticket )
-				{
-					$this->kfem_ticket_display_html( $ID, $ticket, $settings['is_active'] );
-				}
-				if ( count( $tickets ) < 1 ) {
-					$this->kf_links_item_display_html( 0, array( 'ID' => 'new', 'name' => '' ), false );
+				if ( empty( $tickets ) ) {
+					$this->kfem_ticket_display_html( 0, array( 'id' => 'new', 'name' => '' ), false );
+				} else {
+					foreach ( $tickets as $ID => $ticket )
+					{
+						$this->kfem_ticket_display_html( $ID, $ticket, $settings['is_active'] );
+					}
 				}
 				?>
 			</div>
 			<h4><a href="#kf-em-ticket-add" onClick="kfem_createTicket()">+ Weitere Disziplin hinzufügen</a></h4>
-			<input type="hidden" id="kf-em-ticket-counter" value="<?php echo count($tickets); ?>" />
-			<input type="hidden" name="kf-em-tickets-information" id="kf-em-tickets-information" value="<?php echo $settings['string'] ?>" />
+			<input type="hidden" id="kf-em-ticket-counter" value="<?php echo ( empty( $tickets ) ? 1 : count($tickets) ); ?>" >
+			<input type="hidden" name="kf-em-tickets-information" id="kf-em-tickets-information" value="<?php echo $settings['string'] ?>" >
 		</div>
 		<?php
 	}
@@ -73,10 +80,23 @@ class MetaBoxBooking extends MetaBox_Abstract
 		?>
 		<div id="kf-em-ticket-item[<?php echo $ID; ?>]" class="kf-em-ticket-item">
 			<input type="hidden" value="<?php echo $ticket['id']; ?>" />
-			<input onChange="kfem_createBookingString();"<?php if ( $is_active ) echo ' required'; ?> value="<?php echo $ticket['name']; ?>" placeholder="Name" />
-			<input onChange="kfem_createBookingString();"<?php if ($ticket['price'] > 0 ) echo ' value="' . $ticket['price'] . '"'; ?> placeholder="frei" />
-			<input onChange="kfem_createBookingString();"<?php if ($ticket['spots'] > 0 ) echo ' value="' . $ticket['spots'] . '"'; ?> placeholder="unbeschränkt"/>
-			<input onClick="kfem_deleteTicket( 'kf-em-ticket-item[<?php echo $ID; ?>]' ); kfem_createBookingString()" value="&cross;" type="button" class="button button-small button-primary" />
+			<input placeholder="Name"  
+				onChange="kfem_createBookingString();"
+				<?php if ( $is_active ) echo ' required'; ?> 
+				<?php if ( !empty( $ticket['name'] ) ) echo ' value="' . $ticket['name'] . '"'; ?>
+			>
+			<input placeholder="frei" 
+				onChange="kfem_createBookingString();"
+				<?php if ( $ticket['price'] > 0 ) echo ' value="' . $ticket['price'] . '"'; ?>
+			>
+			<input placeholder="unbeschränkt" 
+				onChange="kfem_createBookingString();"
+				<?php if ( !empty( $ticket['spots'] ) ) echo ' value="' . $ticket['spots'] . '"'; ?> 
+			>
+			<input value="&cross;" type="button"
+				class="button button-small button-primary"
+				onClick="kfem_deleteTicket( 'kf-em-ticket-item[<?php echo $ID; ?>]' );kfem_createBookingString()"
+			>
 		</div>
 		<?php
 	}
